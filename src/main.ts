8547b4a606e1ccd304c49ddc6bfd00ef7c8ec509
wiejiechou <wiejie.chou@gmail.com>
2026-04-08 19:14:10 +0800

    feat(wchoice-admin): 加入 [Alex Brush] 字型 (近似於用戶方 logo 上的英文草寫)，用於登入畫面及選單上方的 logo 文字

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,10 @@ import PureDescriptions from "@pureadmin/descriptions";
 
 // 引入重置样式
 import "./style/reset.scss";
-// 引入 Iansui 字体
+// 引入 Iansui 字型
 import "@fontsource/iansui";
+// 引入 Alex Brush 字型
+import "@fontsource/alex-brush";
 // 导入公共样式
 import "./style/index.scss";
 // 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
